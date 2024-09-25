085aee4cc2a52390d586e75f809cba917ee33dbd
krabzholder <krabzholder@gmail.com>
2024-09-24 23:06:00 -0400

    WESRTesg

--- update_pdf_list.py
+++ update_pdf_list.py
@@ -25,9 +25,11 @@ with open('index.html', 'r+') as f:
     # Insert the updated HTML list inside <ul id="pdf-list">
     new_html_content = html_content[:start_pos] + '\n' + html_list + html_content[end_pos:]
 
-    # Update the PDF files array in the script
-    pdf_files_js = ',\n            '.join(pdf_files_array)  # Join into a string for JS
-    new_html_content = new_html_content.replace('const pdfFiles = [\n', f'const pdfFiles = [\n            {pdf_files_js},\n')
+    # Remove the old pdfFiles array and add the new one
+    new_html_content = new_html_content.replace(
+        'const pdfFiles = [',
+        f'const pdfFiles = [\n            {",\n            ".join(pdf_files_array)},\n'
+    )
 
     # Overwrite the file with the updated content
     f.seek(0)
